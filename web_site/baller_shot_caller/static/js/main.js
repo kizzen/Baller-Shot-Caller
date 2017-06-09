333f27eb80e59bf2b142a294dac8c46031ef9b70
andrewhwei <andrew.wei92@yahoo.com>
2017-06-09 14:36:01 -0700

    Removed extraneous DB info, generated filler splash page(?), aws hosting settings started

---
+++ web_site/baller_shot_caller/static/js/main.js
@@ -0,0 +1,130 @@
+$(document).ready(function() {
+
+	// Preloader
+	$(window).load(function(){
+		$('.preloader').fadeOut();
+	});
+
+	// Initiat WOW.js
+	var wow = new WOW(
+	  {
+	  	mobile: false
+	  }
+	);
+	wow.init();
+
+	// .intro-section reduce opacity when scrolling down
+	$(window).scroll(function(){
+		if($(window).width() > 1260) {
+			windowScroll = $(window).scrollTop();
+			contentOpacity = 1 - (windowScroll / ($('#intro').offset().top+$('#intro').height()));
+			$('.intro-section').css('transform','translateY('+Math.floor(windowScroll*0.16)+'px)');
+			$('.intro-section').css('-webkit-transform','translateY('+Math.floor(windowScroll*0.16)+'px)');
+			$('.intro-section').css('opacity',contentOpacity.toFixed(2));
+		}
+	});
+
+	// Fixed navigation
+	$(window).scroll(function() {
+	    if ($(window).scrollTop() > 500) {
+	        $('.navbar').addClass('fixednav');
+	    } else {
+	    	$('.navbar').removeClass('fixednav');
+	    }
+	});
+
+	// Initiat onepageNav.js
+	$('.nav').onePageNav({
+		currentClass: 'current',
+		'scrollOffset': 500
+	});
+
+	// Hide Mobile Nav when clicking item
+	$(".nav a, .navbar-header a").click(function(event) {
+		$(".navbar-collapse").removeClass("in").addClass("collapse");
+	});
+
+	/* Buttons Scroll to Div */
+	$('.navbar-brand').click(function () {
+		$.scrollTo('.intro', 1000);
+	return false;
+	});
+
+	$('.btn-custom').click(function () {
+		$.scrollTo('.download', 1000);
+	return false;
+	});
+
+	$('.btn-custom-border, a.mouse').click(function () {
+		$.scrollTo('.features', 1000);
+	return false;
+	});
+
+	// Screenshot carousel
+	$(".screens").owlCarousel({
+		items: 4,
+		navigation:true,
+		navigationText: [
+			"<i class='fa fa-angle-left btn-slide'></i>",
+			"<i class='fa fa-angle-right btn-slide'></i>"
+			],
+		pagination: false,
+		itemsDesktop: [1000, 4],
+        itemsDesktopSmall: [990, 3],
+        itemsTablet: [600, 1],
+        itemsMobile: false
+	});
+
+	// Screenshot lightbox
+	$('.screens a').nivoLightbox({
+	    effect: 'fadeScale'
+	});
+
+	// Brief carousel
+	$(".small-slider").owlCarousel({
+		items: 1,
+		navigation: true,
+		navigationText: [
+			"<i class='fa fa-angle-left btn-slide'></i>",
+			"<i class='fa fa-angle-right btn-slide'></i>"
+			],
+		pagination: false,
+		itemsDesktop: [1000, 1],
+        itemsDesktopSmall: [900, 1],
+        itemsTablet: [600, 1],
+        itemsMobile: false
+	})
+
+	// Testemonial carousel
+	$(".testemonials").owlCarousel({
+		autoPlay: 8000,
+		autoHeight : true,
+		singleItem: true,
+		navigation: false,
+		itemsDesktop: [1000, 1],
+        itemsDesktopSmall: [900, 1],
+        itemsTablet: [600, 1],
+        itemsMobile: false
+	});
+
+	// Initiat fitVids.js
+	$(".video-item").fitVids();
+
+	// Bootstrap Tab navigation
+	$('.tabs a').click(function (e) {
+		e.preventDefault()
+		$(this).tab('show')
+	});
+
+	// Testemonial carousel
+	$(".customer-slider").owlCarousel({
+		autoPlay: 8000,
+		items: 5,
+		pagination: false,
+		itemsDesktop: [1000, 1],
+        itemsDesktopSmall: [900, 1],
+        itemsTablet: [600, 1],
+        itemsMobile: false
+	});
+ 
+});
--- web_site/baller_shot_caller/urls.py
+++ web_site/baller_shot_caller/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 app_name = 'baller_shot_caller'
 urlpatterns = [
-    url(r'^$', views.homepage, name='homepage'),
-    url(r'^user/(?P<user_id>[0-9]+)/$', views.user, name='user'),
+    url(r'^', views.homepage, name='homepage'),
 ]
